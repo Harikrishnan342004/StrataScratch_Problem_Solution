b16b83e148b5b9c6fa192dbdada6e81b2564c8fc
Hari <harikrishnan53828@gmail.com>
2026-02-03 13:25:44 +0530

    helo

---
+++ Common-Interview-questions/solution.sql
@@ -0,0 +1,51 @@
+-- Find Second highest Salary
+
+-- LIMIT 
+SELECT DISTINCT salary FROM employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1
+
+-- Subquery
+SELECT MAX(Salary)  FROM employee
+WHERE Salary < (SELECT MAX(Salary) FROM employee);
+
+-- Windows Function
+SELECT * FROM
+(
+    SELECT * , DENSE_RANK() OVER( ORDER BY Salary DESC ) AS rnk 
+    FROM employee
+)t
+WHERE rnk = 2;
+
+-------------------------------------------------------------------------------------------
+
+-- Find third highest Salary
+
+-- LIMIT 
+SELECT DISTINCT salary FROM employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 2
+
+-- Subquery
+SELECT MAX(Salary)  FROM employee
+WHERE  Salary < (SELECT MAX(Salary) FROM employee) 
+WHERE Salary < (SELECT MAX(Salary) FROM employee);
+
+-- Windows Function
+SELECT * FROM
+(
+    SELECT * , DENSE_RANK() OVER( ORDER BY Salary DESC ) AS rnk 
+    FROM employee
+)t
+WHERE rnk = 3;
+
+----------------------------------------------------------------------------------------------------
+-- Common Interview Starter
+
+SELECT * FROM 
+(
+    SELECT * , ROW_NUMBER() OVER(PARTITION BY email ORDER by id ) AS rnk FROM users
+)t
+WHERE rnk > 1;
+
+
