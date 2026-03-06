d2d707b5397dfe7325ad73930507430d390ee302
Hari <harikrishnan53828@gmail.com>
2026-03-06 22:27:32 +0530

    helo

--- mongoDB/CURD OPERATION/CREATE/create.js
+++ mongoDB/CURD OPERATION/CREATE/create.js
@@ -2,29 +2,29 @@ db.createCollection("capped" , {capped : true , size: 10000 , max: 3 })
 
 db.capped.insertOne( {name : "Hari"} )
 
-{
-  acknowledged: true,
-  insertedId: ObjectId('69a899ffa51ffb3cd47c2907')
-}
+// {
+//   acknowledged: true,
+//   insertedId: ObjectId('69a899ffa51ffb3cd47c2907')
+// }
 
 db.capped.insertOne( {name : "Hari1"} )
 
-{
-  acknowledged: true,
-  insertedId: ObjectId('69a89a08a51ffb3cd47c2908')
-}
+// {
+//   acknowledged: true,
+//   insertedId: ObjectId('69a89a08a51ffb3cd47c2908')
+// }
 
 db.capped.insertOne( {name : "Hari2"} )
 
-{
-  acknowledged: true,
-  insertedId: ObjectId('69a89a0ea51ffb3cd47c2909')
-}
+// {
+//   acknowledged: true,
+//   insertedId: ObjectId('69a89a0ea51ffb3cd47c2909')
+// }
 
 db.capped.find()
 
-[
-  { _id: ObjectId('69a899ffa51ffb3cd47c2907'), name: 'Hari' },
-  { _id: ObjectId('69a89a08a51ffb3cd47c2908'), name: 'Hari1' },
-  { _id: ObjectId('69a89a0ea51ffb3cd47c2909'), name: 'Hari2' }
-]
+// [
+//   { _id: ObjectId('69a899ffa51ffb3cd47c2907'), name: 'Hari' },
+//   { _id: ObjectId('69a89a08a51ffb3cd47c2908'), name: 'Hari1' },
+//   { _id: ObjectId('69a89a0ea51ffb3cd47c2909'), name: 'Hari2' }
+// ]
