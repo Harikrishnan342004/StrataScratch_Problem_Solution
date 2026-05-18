c611bbca6644fa61d093514ba121097cc2fae76b
Harikrishnan342004 <harikrishnan53828@gmail.com>
2026-05-18 21:04:21 +0530

    hello

--- stata scratch/Easy/Find all inspections which are part of an inactive program/test.sql
+++ stata scratch/Easy/Find all inspections which are part of an inactive program/test.sql
@@ -1,2 +1,31 @@
 select * from los_angeles_restaurant_health_inspections
 WHERE program_status = 'inactive';
+
+
+-- Find all inspections which are part of an inactive program
+
+
+-- Table
+-- los_angeles_restaurant_health_inspections
+
+
+-- activity_date:       date
+-- employee_id:         text
+-- facility_address:    text
+-- facility_city:       text
+-- facility_id:         text
+-- facility_name:       text
+-- facility_state:      text
+-- facility_zip:        text
+-- grade:               text
+-- owner_id:            text
+-- owner_name:          text
+-- pe_description:      text
+-- program_element_pe:  bigint
+-- program_name:        text
+-- program_status:      text
+-- record_id:           text
+-- score:               bigint
+-- serial_number:       text
+-- service_code:        bigint
+-- service_description: text
